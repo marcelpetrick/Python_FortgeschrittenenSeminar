890f45347528488e6f2dad9f15a1b8c5ddfcba53
mail@marcelpetrick.it <mail@marcelpetrick.it>
2018-05-07 16:25:52 +0200

    current state at the end of the day

--- venv/day1/m06_kalender.py
+++ venv/day1/m06_kalender.py
@@ -1,10 +1,15 @@
 # wir bauen jetzt etwas nach, was fast schon so ähnlich in Date von datetime steckt ..
 class Kalender(object):
     # Klasseneigenschaft
-    # tuple um die anzahl der tage zu definieren; gehört zu klasse und nicht den objekten selber
+    # Tupel um die Anzahl der Tagezu definieren; gehört zu Klasse und nicht den Objekten selber
     # Februar bekommt in der ersten Version ersteinmal 28 ... wird später korrigiert
     tageProMonat = (-42, 31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31) # first element as placeholder, so that all elements shift by one to the right
     # or replace -42 by None
+
+    # Statische Methode:
+    @staticmethod
+    def istSchaltjahr(jahr):
+        return (jahr % 400 == 0) or (jahr % 100 != 0 and jahr % 4 == 0)
 
     # Konstruktormethode (eine spezielle Objektmethode)
     def __init__(self, tag, monat, jahr):
@@ -19,14 +24,16 @@ class Kalender(object):
     def nextDay(self):
         # todo später schaltjahre berücksichtigen
         self.tag += 1
-        if self.tag > Kalender.tageProMonat[self.monat]: # todo Monate haben unterschiedliche Anzahl ..
+        # ternärer Operator
+        korrektur = 1 if self.monat == 2 and Kalender.istSchaltjahr(self.jahr) else 0 # nicht mit: frage ? true : false
+        if self.tag > Kalender.tageProMonat[self.monat] + korrektur: # todo Februar /Schaltjahre
             self.tag = 1
             self.monat += 1
             if self.monat == 13:
                 self.monat = 1
                 self.jahr += 1
 
-    # Magische MEthode (spezielle Objektmethode)
+    # Magische Methode (spezielle Objektmethode)
     def __repr__(self):
         return "%02i.%02i.%04i" % (self.tag, self.monat, self.jahr)
 
