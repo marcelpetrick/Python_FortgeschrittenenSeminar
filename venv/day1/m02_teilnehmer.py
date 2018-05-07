f8f8c67f464b95160094c000d4a6a940f34d4dcd
mail@marcelpetrick.it <mail@marcelpetrick.it>
2018-05-07 10:23:39 +0200

    neues file

---
+++ venv/day1/m02_teilnehmer.py
@@ -0,0 +1,12 @@
+# was ist eine klasse? mit eigenen worten?
+# typ mit individuellen methoden und daten(feldern)
+
+
+# Thomas, 2.2.70, Bielefeld, C, Java
+# Marcel, 1.1.83, Kiel, C++, Bash
+# Henning, 16.1.60, Hildesheim, Assembler, RexX
+# Martin, 3.3.87, Sindelfingen, Matlab
+# Matthias, 18.8.89, Lichtenau, Matlab
+# Daniel, 1.1.80, Bielefeld, Python, Java
+
+
