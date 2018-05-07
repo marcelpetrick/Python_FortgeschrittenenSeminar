cd285a7c09aff94b7af5cd0761a96b65b42dbce5
mail@marcelpetrick.it <mail@marcelpetrick.it>
2018-05-07 15:26:38 +0200

    new eccfeption added to handle the initialization with wrong values

--- venv/day1/m05_kapselung.py
+++ venv/day1/m05_kapselung.py
@@ -1,2 +1,37 @@
+
+
 class Teilnehmer(object):
-    def __init__(self), :
+    def __init__(self, vorname, handicap):
+
+        if not(Teilnehmer.validHandicap(handicap)):
+            raise Exception("Invalid Initialization of Teilnehmer instance") #or return ...
+        self.vorname = vorname
+        self.handicap = handicap
+
+    # klassen oder grundsätzliche frage?
+    # hängt nicht von eigenschaften des teilnehmers ab - sondern eher grundsätzlich
+    # Objekt- und Klassenmethoden
+    @staticmethod
+    def validHandicap(handicap): #ist Klassen (statische) Methode - self fällt weg
+        return handicap > 0 # wenn also positiv, dann zulässig
+
+    def __getHandicap(self):
+        return self.__getHandicap
+
+    def __setHandicap(self, handicap):
+        self.__getHandicap = handicap
+
+    handicap = property(__getHandicap, __setHandicap)
+
+# hier noch falsch, weil ohne prüfungen implementiert - aber wenn man später den direkten Zugriff auf Proper
+try:
+    anna =  Teilnehmer("Anna", -7)
+except Exception:
+    print("Bockmist")
+
+anna.handicap = -42
+anna.vorname = "Gabi"
+anna.vornamen = "peter" # geht, aber sollte liebernicht so ausgeführt werden können
+# dies kann man über slots machen - und darüber prüfen, ob gültige Methode
+
+print(Teilnehmer.validHandicap(anna.handicap))
