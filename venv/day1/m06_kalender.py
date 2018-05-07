03c4c06071b28adb2a50241ad80b6f559d692322
mail@marcelpetrick.it <mail@marcelpetrick.it>
2018-05-07 15:56:07 +0200

    working nextday() and some printing with the __repr_-method

--- venv/day1/m06_kalender.py
+++ venv/day1/m06_kalender.py
@@ -7,6 +7,28 @@ class Kalender(object):
         self.monat = monat
         self.jahr = jahr
 
+    # go to the next day; Aufforderung als Nachricht
+    def nextDay(self):
+        # todo später schaltjahre berücksichtigen
+        self.tag += 1
+        if self.tag == 32: # todo Monate haben unterschiedliche Anzahl ..
+            self.tag = 1
+            self.monat += 1
+            if self.monat == 13:
+                self.monat = 1
+                self.jahr += 1
+
+    def __repr__(self):
+        return "%02i.%02i.%04i" % (self.tag, self.monat, self.jahr)
+
 k = Kalender(31,12,2017)
 
+print(k)
+k.nextDay()
+print(k)
 
+print("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~")
+
+for t in range(10):
+    print(k)
+    k.nextDay()
