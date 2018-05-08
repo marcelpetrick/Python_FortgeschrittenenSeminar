4b26e5c2dd66357411320e91e9d271120d9f4f55
mail@marcelpetrick.it <mail@marcelpetrick.it>
2018-05-08 15:49:49 +0200

    implementation of the kwargs-usage; and slots

--- venv/day2/m11_slots.py
+++ venv/day2/m11_slots.py
@@ -7,11 +7,12 @@ ausgabe(vorname="Anna", alter=42)
 # Jetzt ohne etwas zu benennen alles als Parameter aufnehmen :)
 class Sportwagen(object):
     __slots__ = ['marke', 'modell', 'ps'] # kann man auch als Tuple nutzen, also mit (..)
- #   def __init__(self, **kwargs): # keyword arguments
-  #      pass
-        #self.marke = marke
-        #self.modell = modell
-        #self.ps = ps
+    def __init__(self, **kwargs): # keyword arguments
+        for key in kwargs:
+            # wie kann man einem Objekt seine Eigenschaften zuweisen? Vielleicht über eine magische Methode?
+            self.__setattr__(key, kwargs[key])
+            # jetzt hätte man aber noch pobleme, wenn weniger als drei Argumente zum Beispiel übergeben werden
+            # Dafür kann man aber auch einmal über __slots__ iterieren und diese dann mit Standardwerten belegen
 
-#s = Sportwagen("Lambo", "Gallardo", 400)
-#s.verbrauch = 42
+s = Sportwagen(marke="Lambo", modell="Gallardo", ps=400)
+#s.verbrauch = 42 # wäre jetzt ein AttributeError
