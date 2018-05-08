5c84189fedf7d5129187da3dbd294e730a076b82
mail@marcelpetrick.it <mail@marcelpetrick.it>
2018-05-08 15:32:40 +0200

    slots: new example

---
+++ venv/day2/m10_slots.py
@@ -0,0 +1,9 @@
+# Slots?!?
+
+class Sportwagen(object):
+    def __init__(self, marke, modell, ps):
+        self.marke = marke
+        self.modell = modell
+        self.ps = ps
+
+s = Sportwagen("Lambo", "Gallardo", 400)
