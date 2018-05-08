673b1c52941d47d0a180aead8b35374d52b975e1
mail@marcelpetrick.it <mail@marcelpetrick.it>
2018-05-08 13:18:24 +0200

    intermediate

--- venv/day2/m09_brueche.py
+++ venv/day2/m09_brueche.py
@@ -3,9 +3,19 @@
 # Plan: Elemente der Datenstrukturen auch in natürlicher Weise nutzen können.
 # Natürlich objekt-orientiert.
 
-def Bruch(object):
-    pass
+class Bruch(object):
+    def __init__(self, z, n):
+        '''
+        :param z: Zähler
+        :param n: Nenner
+        '''
+        self.z = z
+        self.n = n
+
+    def __mul__(self, other):
+        return 42
 
 a = Bruch(3, 4) # zum Anlegen muss man leider dann auch diese Notation benutzen; für die Initialisierung
 b = Bruch(1, 8)
 c = a * b # multiplikation jetzt als einfachste Funktion
+print(c)
