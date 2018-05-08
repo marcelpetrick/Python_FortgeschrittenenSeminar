d408dfcdb0a97857ea6065e9ab269a26ae6929bb
mail@marcelpetrick.it <mail@marcelpetrick.it>
2018-05-08 11:46:56 +0200

    type-check-decorater working: is possible because all "things" are classes

--- venv/day2/m08_type_check_decoration.py
+++ venv/day2/m08_type_check_decoration.py
@@ -1,18 +1,26 @@
+def intarg(f):
+    def g(n):
+        if type(n) == int:
+            return f(n)
+        else:
+            raise TypeError()
+    return g
+
 # goal: add some check for the type (is number?) and if positive
 # duck typing: alles was wie eine Ente watschelt, quakt und fliegt ist eine Ente (für mich)
 # eigentlich will man es gar nicht, dass man auf Typen prüft (pythonisch unüblich)
+@intarg
 def fakultaet_rekursiv(n):
-    # print(type(n)) # chould have been done. But if G. v. Rossum wanted to have a type-check, he would have implemented it
-    if type(n) == int: # this is not pythonic
-        if n == 0:
-            return 1
-        else:
-            return n * fakultaet_rekursiv(n - 1)
+    if n == 0:
+        return 1
     else:
-        raise TypeError()
+        return n * fakultaet_rekursiv(n - 1)
 
 # ~~~~~~~~~~~ calls ~~~~~~~~~~~~~~~~~
 result = fakultaet_rekursiv(4)
 print(result)
 result = fakultaet_rekursiv("xx")
 print(result) # results: "None" - not a bug
+
+# my idea: chould be really helpful to add in quick time some output for debugging (print output of the results for function-calls)
+# define decorater as printing of call-param plus result - just add via @ to the different functions
