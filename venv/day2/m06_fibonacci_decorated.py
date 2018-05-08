28d8eeffb820105781a256e44e6145a3b348970a
mail@marcelpetrick.it <mail@marcelpetrick.it>
2018-05-08 11:13:00 +0200

    intermediate: other way to decorate

--- venv/day2/m06_fibonacci_decorated.py
+++ venv/day2/m06_fibonacci_decorated.py
@@ -1,4 +1,5 @@
 # Der Parameter funk ist die zu dekorierende Funktion
+# Nur die Definition wird genutzt. Es ist KEIN Aufruf!
 def dekoriere(funk):
     d = {} # leeres Wörterbuch
     def dekorierte_funk(n):
@@ -9,11 +10,16 @@ def dekoriere(funk):
 
     return dekorierte_funk # neue Funktion wird auch zurükgegeben; Rückgabe ist die dekorierte Funktion
 
+# the following line is a direct annotation for the decoration; could also be multiple decorations - separated by comma
+@dekoriere
 def fib(n):
     if n in (0,1):
         return 1
     return fib(n-2) + fib(n-1) #else vermieden: Einrückungstiefe gespart
 
-fib = dekoriere(fib)
+# can be replaced by the decoration in line 13
+#fib = dekoriere(fib)
 
-print(fib(50))
+print(fib(50)) # eventuell einmal mit pythontutor parsen
+
+# closure als kontext des funktionsaufrufes
