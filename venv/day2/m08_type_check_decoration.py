0745d7cc12c5aaccefcc6a3fc14c071b704f27b6
mail@marcelpetrick.it <mail@marcelpetrick.it>
2018-05-08 11:40:32 +0200

    type-checking implemented, but not really recommended

--- venv/day2/m08_type_check_decoration.py
+++ venv/day2/m08_type_check_decoration.py
@@ -1,7 +1,18 @@
 # goal: add some check for the type (is number?) and if positive
+# duck typing: alles was wie eine Ente watschelt, quakt und fliegt ist eine Ente (für mich)
+# eigentlich will man es gar nicht, dass man auf Typen prüft (pythonisch unüblich)
 def fakultaet_rekursiv(n):
-    #if type(n) == int # chould have been done. But if G. v. Rossum wanted to have a type-check, he would have implemented it
-    if n == 0:
-        return 1
+    # print(type(n)) # chould have been done. But if G. v. Rossum wanted to have a type-check, he would have implemented it
+    if type(n) == int: # this is not pythonic
+        if n == 0:
+            return 1
+        else:
+            return n * fakultaet_rekursiv(n - 1)
     else:
-        return n * fakultaet_rekursiv(n - 1)
+        raise TypeError()
+
+# ~~~~~~~~~~~ calls ~~~~~~~~~~~~~~~~~
+result = fakultaet_rekursiv(4)
+print(result)
+result = fakultaet_rekursiv("xx")
+print(result) # results: "None" - not a bug
