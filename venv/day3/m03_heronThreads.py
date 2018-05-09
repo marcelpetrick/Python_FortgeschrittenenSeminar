bdc5d86a66b260523f61fe00ecaa0203253e3d8c
mail@marcelpetrick.it <mail@marcelpetrick.it>
2018-05-09 09:45:15 +0200

    working synch with sleep (..) or not

--- venv/day3/m03_heronThreads.py
+++ venv/day3/m03_heronThreads.py
@@ -1,5 +1,7 @@
 from threading import _start_new_thread
+from time import sleep
 
+# globale Variable
 num_threads = 0
 
 
@@ -8,6 +10,7 @@ def heron(a):
     num_threads += 1
 
     sleep(3)
+    print(a)
 
     num_threads -= 1
     return 42
@@ -18,6 +21,8 @@ _start_new_thread(heron, (99,))
 _start_new_thread(heron, (999,))
 _start_new_thread(heron, (1733,))
 _start_new_thread(heron, (17334,))
+
+#sleep(3) # if removed, then the output is shown
 
 while num_threads > 0:
     pass
