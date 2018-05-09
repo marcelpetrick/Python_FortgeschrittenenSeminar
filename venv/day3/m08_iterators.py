178ccd12fcc2f0c3320594196cd17066ce51ce57
mail@marcelpetrick.it <mail@marcelpetrick.it>
2018-05-09 13:47:22 +0200

    fibonacci-iterator

---
+++ venv/day3/m08_iterators.py
@@ -0,0 +1,28 @@
+from collections import Iterator
+
+#-------------------------------------------
+
+class Fib(Iterator):
+    def __init__(self):
+        self.a = 1
+        self.b = 1
+
+    # Plan: das Objekt ist selber der Iterator!
+    def __iter__(self):
+        return self
+
+    # override!
+    def __next__(self):
+        self.a, self.b = self.b, self.a + self.b
+        return self.b
+
+#-------------------------------------------
+fib = Fib()
+print(fib.__next__())
+print(fib.__next__())
+print(fib.__next__())
+print(fib.__next__())
+
+# infinite return ...
+for x in Fib():
+    print(x)
